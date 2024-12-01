bf2a6ddcbcf416a394cd6d3524872c04c54bb6c1
Spudgun <jared.rj.white@gmail.com>
2024-12-01 17:26:11 +0000

    feat: ability to input numers from the pad interface

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let num1 = 0;
-let operator = 0;
-let num2 = 0;
+let num1 = null;
+let operator = null;
+let num2 = null;
 
 let buttons = document.querySelectorAll('.button');
 const screen = document.getElementById('screen-text');
 
 buttons.forEach((element) => {
 	let buttonData = element.dataset;
-	console.log(buttonData);
 	element.addEventListener('click', function () {
 		if (buttonData.type == 'clear') {
 			handleClearButton(buttonData.value);
+		} else if (buttonData.type == 'num') {
+			handleNumberInput(buttonData.value);
+		} else if (buttonData.type == 'operator') {
 		}
+		test();
 	});
 });
 
@@ -19,23 +22,33 @@ function handleClearButton(value) {
 	if (value == 'ac') {
 		resetCalculator();
 	} else {
-		if (num2 != 0) {
-			num2 = 0;
-			setScreenText();
-		} else if (operator != 0) {
+		if (typeof num2 != 'undefined') {
+			num2 = null;
+			setScreenText(operator);
+		} else if (typeof operator != 'undefined') {
+			operator = null;
 			setScreenText(num1);
 		} else {
+			num1 = null;
 			setScreenText();
 		}
 	}
 }
 
+function handleNumberInput(value) {
+	if (operator !== null) {
+		num2 = num2 === null ? value : num2 + value;
+		screen.innerHTML = num2;
+	} else {
+		num1 = num1 === null ? value : num1 + value;
+		screen.innerHTML = num1;
+	}
+	screen.style.opacity = 1;
+}
+
 function operation(num) {}
 
 function resetCalculator() {
-	num1 = 0;
-	operator = '+';
-	num2 = 0;
 	setScreenText();
 }
 
@@ -45,4 +58,8 @@ function setScreenText(text) {
 		screen.style.opacity = 0.5;
 	}
 	screen.innerHTML = text;
+}
+
+function test() {
+	console.log(typeof num1, num1, typeof operator, operator, typeof num2, num2);
 }
